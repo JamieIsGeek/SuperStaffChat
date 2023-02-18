6dbfb3753e84bc52639bb167c95c9952a04bec35
JamieOnSootMC <jamie@sootmc.net>
2023-02-18 18:51:42 +0000

    Initial commit

---
+++ src/main/java/uk/jamieisgeek/superstaffchat/SuperStaffChat.java
@@ -0,0 +1,17 @@
+package uk.jamieisgeek.superstaffchat;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SuperStaffChat extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
